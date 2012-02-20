57cf033b5c13b97349443e007438ef7d38bd2ca0
Eric Kok <erickok@gmail.com>
2012-02-20 11:56:54 +0100

    Added Shazam song share support (issue 193).

--- android/src/org/transdroid/gui/search/SendIntentHelper.java
+++ android/src/org/transdroid/gui/search/SendIntentHelper.java
@@ -13,6 +13,8 @@ public class SendIntentHelper {
 
 	private static final String SOUNDHOUND1 = "Just used #SoundHound to find ";
 	private static final String SOUNDHOUND1_END = " http://";
+	private static final String SHAZAM = "I just used Shazam to discover ";
+	private static final String SHAZAM_END = ". http://";
 	private static final String YOUTUBE_ID= "Watch \"";
 	private static final String YOUTUBE_START = "\"";
 	private static final String YOUTUBE_END = "\"";
@@ -28,6 +30,10 @@ public class SendIntentHelper {
 			// Soundhound song/artist share
 			if (text.startsWith(SOUNDHOUND1)) {
 				return cutOut(text, SOUNDHOUND1, SOUNDHOUND1_END).replace(" by ", " ");
+			}
+			// Shazam song share
+			if (text.startsWith(SHAZAM)) {
+				return cutOut(text, SHAZAM, SHAZAM_END).replace(" by ", " ");
 			}
 			// YouTube app share (stores title in EXTRA_SUBJECT)
 			if (intent.hasExtra(Intent.EXTRA_SUBJECT)) {
