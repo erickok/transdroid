61fd105b8e87cf931e663a222563654e8e9801f6
Eric Kok <erickok@gmail.com>
2013-02-27 13:27:07 +0100

    Started Transdroid Lite project, which also serves as new UI development project.

---
+++ lite/src/org/transdroid/lite/gui/Torrents.java
@@ -0,0 +1,13 @@
+package org.transdroid.lite.gui;
+
+import org.androidannotations.annotations.EActivity;
+import org.androidannotations.annotations.OptionsMenu;
+import org.transdroid.lite.R;
+
+import com.actionbarsherlock.app.SherlockFragmentActivity;
+
+@EActivity(R.layout.activity_torrents)
+@OptionsMenu(R.menu.torrents)
+public class Torrents extends SherlockFragmentActivity {
+
+}
