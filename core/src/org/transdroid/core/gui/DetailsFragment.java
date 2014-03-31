erickok/transdroid

commit 07d169c53289d071744a24116111f3d8319da52f

Prevent crash when labels are not yet loaded yet the label dialog is somehow started (race condition?).

core/src/org/transdroid/core/gui/DetailsFragment.java
@@ -320,8 +320,9 @@ public class DetailsFragment extends SherlockFragment implements OnTrackersUpdat
 
 	@OptionsItem(resName = "action_setlabel")
 	protected void setLabel() {
-		new SetLabelDialog().setOnLabelPickedListener(this).setCurrentLabels(currentLabels)
-				.show(getFragmentManager(), "SetLabelDialog");
+		if (currentLabels == null)
+			new SetLabelDialog().setOnLabelPickedListener(this).setCurrentLabels(currentLabels)
+					.show(getFragmentManager(), "SetLabelDialog");
 	}
 
 	@OptionsItem(resName = "action_forcerecheck")

core/res/layout/actionbar_serverstatus.xml
@@ -81,7 +81,7 @@
 
 		<TextView
 			android:id="@+id/upspeed_text"
-			android:layout_width="wrap_content"
+			android:layout_width="@dimen/ui_serverstatus_width"
 			android:layout_height="wrap_content"
 			android:layout_alignLeft="@id/upcount_text"
 			android:layout_alignParentRight="true"
@@ -94,7 +94,7 @@
 
 		<TextView
 			android:id="@+id/downspeed_text"
-			android:layout_width="wrap_content"
+			android:layout_width="@dimen/ui_serverstatus_width"
 			android:layout_height="wrap_content"
 			android:layout_alignLeft="@id/downcount_text"
 			android:layout_alignRight="@id/downcount_sign"

core/src/org/transdroid/core/gui/TorrentsFragment.java
@@ -318,7 +318,8 @@ public class TorrentsFragment extends SherlockFragment implements OnLabelPickedL
 				return true;
 			} else if (itemId == R.id.action_setlabel) {
 				lastMultiSelectedTorrents = checked;
-				new SetLabelDialog().setOnLabelPickedListener(TorrentsFragment.this).setCurrentLabels(currentLabels)
+				if (currentLabels != null)
+					new SetLabelDialog().setOnLabelPickedListener(TorrentsFragment.this).setCurrentLabels(currentLabels)
 						.show(getFragmentManager(), "SetLabelDialog");
 				mode.finish();
 				return true;

commit 3f5954f765a3c4a3c479dc7cdc1d93e722362fb2

Fixes #122, reversing the painful mistake introduced in 07d169c5 with a == instead of != comparison... Oops!

core/src/org/transdroid/core/gui/DetailsFragment.java
@@ -320,7 +320,7 @@ public class DetailsFragment extends SherlockFragment implements OnTrackersUpdat
 
 	@OptionsItem(resName = "action_setlabel")
 	protected void setLabel() {
-		if (currentLabels == null)
+		if (currentLabels != null)
 			new SetLabelDialog().setOnLabelPickedListener(this).setCurrentLabels(currentLabels)
 					.show(getFragmentManager(), "SetLabelDialog");
 	}

core/src/org/transdroid/core/gui/TorrentsActivity.java
@@ -44,31 +44,18 @@ import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.impl.cookie.BasicClientCookie;
 import org.transdroid.core.R;
-import org.transdroid.core.app.search.SearchHelper_;
-import org.transdroid.core.app.settings.ApplicationSettings;
-import org.transdroid.core.app.settings.ServerSetting;
-import org.transdroid.core.app.settings.SystemSettings;
-import org.transdroid.core.app.settings.SystemSettings_;
-import org.transdroid.core.app.settings.WebsearchSetting;
+import org.transdroid.core.app.search.*;
+import org.transdroid.core.app.settings.*;
 import org.transdroid.core.gui.lists.LocalTorrent;
 import org.transdroid.core.gui.lists.NoProgressHeaderTransformer;
 import org.transdroid.core.gui.lists.SimpleListItem;
-import org.transdroid.core.gui.log.Log;
-import org.transdroid.core.gui.log.Log_;
-import org.transdroid.core.gui.navigation.FilterListAdapter;
-import org.transdroid.core.gui.navigation.FilterListAdapter_;
-import org.transdroid.core.gui.navigation.FilterListDropDownAdapter;
-import org.transdroid.core.gui.navigation.FilterListDropDownAdapter_;
-import org.transdroid.core.gui.navigation.Label;
-import org.transdroid.core.gui.navigation.NavigationFilter;
-import org.transdroid.core.gui.navigation.NavigationHelper;
-import org.transdroid.core.gui.navigation.RefreshableActivity;
-import org.transdroid.core.gui.navigation.StatusType;
-import org.transdroid.core.gui.rss.RssfeedsActivity_;
+import org.transdroid.core.gui.log.*;
+import org.transdroid.core.gui.navigation.*;
+import org.transdroid.core.gui.rss.*;
 import org.transdroid.core.gui.search.BarcodeHelper;
 import org.transdroid.core.gui.search.FilePickerHelper;
 import org.transdroid.core.gui.search.UrlEntryDialog;
-import org.transdroid.core.gui.settings.MainSettingsActivity_;
+import org.transdroid.core.gui.settings.*;
 import org.transdroid.core.service.BootReceiver;
 import org.transdroid.core.service.ConnectivityHelper;
 import org.transdroid.core.widget.ListWidgetProvider;